a354971aabcb66a7ca9df03a855ad8f6c21dcf5a
Alexander Luisi <al3515@ic.ac.uk>
2016-07-28 12:14:33 +0100

    Exception and Logs updates

---
+++ WebAPI Trial/WebAPI Trial/App_Start/CentralExceptionHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Handlers;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Web.Services;
+
+
+public class CentralExceptionHandler : ExceptionHandler
+{
+
+    public override void Handle(ExceptionHandlerContext myContext)
+    {
+
+        myContext.Result = new TextPlainErrorResult
+        {
+            Request = myContext.ExceptionContext.Request,
+            myContent = "Something Went Wrong"
+
+        };
+
+    }
+
+    private class TextPlainErrorResult : IHttpActionResult
+    {
+
+        public HttpRequestMessage Request { get; set; }
+
+        public string myContent { get; set; }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken myCancelToken)
+        {
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            response.Content = new StringContent(myContent);
+            response.RequestMessage = Request;
+            return Task.FromResult(response);
+
+        }
+    }
+}
---
+++ WebAPI Trial/WebAPI Trial/App_Start/TraceExceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Handlers;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Web.Services;
+using System.Diagnostics;
+
+public class TraceExceptions : ExceptionLogger
+{
+    public override void Log(ExceptionLoggerContext context)
+    {
+        Trace.TraceError(context.ExceptionContext.Exception.ToString());
+    }
+}

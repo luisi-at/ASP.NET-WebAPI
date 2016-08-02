0117d2d770bf7cc8d6ea81967e70475f7008a6af
Alexander Luisi <al3515@ic.ac.uk>
2016-08-02 09:50:27 +0100

    Cleaned code and commented

--- WebAPI Trial/WebAPI Trial/Controllers/MainController.cs
+++ WebAPI Trial/WebAPI Trial/Controllers/MainController.cs
@@ -14,66 +14,87 @@ namespace WebAPI_Trial.Controllers
 {
     public class MainController : ApiController
     {
+
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+        // Start of Autofac stuff
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+
+
+        // This integrates Autofac with WebAPI by not using Service Locators
         private readonly IDateWriter _dateWriter;
         public MainController(IDateWriter datewriter)
         {
+            // set the variable to hold the value
             _dateWriter = datewriter;
         }
-        //^^^^ This integrates Autofac with WebAPI better than the method seen below not using service locators
-
+       
         public sealed class TodayDate : IDateWriter
         {
             private IOutput _output;
             public TodayDate(IOutput output)
             {
-                this._output = output;
+                // set the variable to hold the value
+                _output = output;
             }
             public string WriteDate()
             {
-                //this._output.Write(DateTime.Today.ToLongDateString());
-                return DateTime.Today.ToLongDateString();
+               // get the value to return up the call path
+               return DateTime.Today.ToLongDateString();
             }
         }
 
+        public interface IOutput
+        {
+            //prototype the output functions here under this interface
+            string Write(string content);
+        }
+
+        public interface IDateWriter
+        {
+            //method to give the final output that can be invoked by those classes who interface with IDateWriter
+            string WriteDate();
+        }
+
+        public sealed class DateOutput : IOutput
+        {
+            public string Write(string content)
+            {
+                //method to fetch the date and can be invoked by those classes who interface with IOutput
+                return content;
+            }
+        }
+
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+        // End of Autofac stuff
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+        // Note: To implement writing a different date, the only thing that needs to be done is create...
+        // ...a different IDateWriter Class, such as TomorrowDate and then change the registartion in...
+        // ...the application start config file. There is no need to change any existing classes!
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+
+
         private static IContainer Container { get; set; }
         // GET: api/Main
+        // just to return some values from a class
         public Something Get()
         {
             //throw new ArgumentNullException("Something");
             
-            return new Something { type = "SomeType", model = "SomeModel", color = "Colour" };
+            return new Something { Type = "SomeType", Model = "SomeModel", Color = "Colour" };
         }
 
         // GET: api/Main/5
         public string Get(int id) 
         {
-            
-            //CallContext.LogicalSetData("MyData", "Some Data");
-            
-            //var myData = CallContext.LogicalGetData("DATA");
-            
-            //use autofac to return todays date here to the http response
-            //var builder = new ContainerBuilder();
-            //builder.RegisterType<dateOutput>().As<IOutput>();
-            //builder.RegisterType<TodayDate>().As<IDateWriter>();
-            //Container = builder.Build();
-
-            //Need to figure out how to return the value of the string so it can be output on the http message
-            //var myData = WriteDate();
+            //invokes the method to return the current date
             var myData = _dateWriter.WriteDate();
-
+            //returns the value
             return myData;
-        }
-
-        public string WriteDate()
-        {
-            ////service locator- an anti pattern that is best not used
-            //using (var scope = Container.BeginLifetimeScope())
-            //{
-            //    var writer = scope.Resolve<IDateWriter>();
-            //    return writer.WriteDate();
-            //}
-            return null;
         }
 
         public class ValuesController : ApiController
@@ -107,33 +128,8 @@ namespace WebAPI_Trial.Controllers
 
     public sealed class Something
     {
-        public string type { get; set; }
-        public string model { get; set; }
-        public string color { get; set; }
-    }
-
-    //------------------------
-    //Autofac stuff
-    //------------------------
-
-    public interface IOutput
-    {
-        //prototype the output functions here under this interface
-        string Write(string content);
-    }
-
-    public interface IDateWriter
-    {
-        string WriteDate();
-    }
-
-    public sealed class DateOutput : IOutput
-    {
-        public string Write(string content)
-        {
-            return content;
-        }
-    }
-
-    
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+    }    
 }
